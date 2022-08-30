9c6db6f604051cf16fe2c70cda9e5c199ab49c6a
Benjamin Soltoff <ben@bensoltoff.com>
2022-08-30 10:06:08 -0400

    Add across() function to iteration exercises

--- 02-iteration-solution.R
+++ 02-iteration-solution.R
@@ -1,45 +1,50 @@
 library(tidyverse)
+library(modeldata)
+library(rcis)
 
 # For loops
-## Write a for loop that calculates the arithmetic mean for every column in mtcars
-as_tibble(mtcars)
+## Write a for loop that calculates the arithmetic mean for every column in car_prices
+car_prices
 
 ### preallocate space for the output
-output <- vector("numeric", ncol(mtcars))
+output <- vector("numeric", ncol(car_prices))
 
-### initialize the loop along all the columns of mtcars
-for(i in seq_along(mtcars)){
+### initialize the loop along all the columns of car_prices
+for(i in seq_along(car_prices)){
   # calculate the mean value for the i-th column
-  output[[i]] <- mean(mtcars[[i]], na.rm = TRUE)
+  output[[i]] <- mean(car_prices[[i]], na.rm = TRUE)
 }
 
 output
 
-## Write a for loop that calculates the maximum value in each column of diamonds
-diamonds
+## Write a for loop that calculates the maximum value in each column of car_prices
+car_prices
 
 ### preallocate space for the output
-output <- vector("numeric", ncol(diamonds))
+output <- vector("numeric", ncol(car_prices))
 
-### initialize the loop along all the columns of diamonds
-for(i in seq_along(diamonds)){
+### initialize the loop along all the columns of car_prices
+for(i in seq_along(car_prices)){
   # calculate the max value for the i-th column
-  output[i] <- max(diamonds[[i]])
+  output[i] <- max(car_prices[[i]])
 }
 
 output
 
 # Write iterative operations using a map() function
-## Write a map() function that calculates the arithmetic mean for every column in mtcars
-as_tibble(mtcars)
+## Write a map() function that calculates the arithmetic mean for every column in car_prices
+map_dbl(car_prices, mean)
 
-map_dbl(mtcars, mean)
+## Write a map() function that calculates the maximum value in each column of car_prices
+map_dbl(car_prices, max)
 
-## Write a map() function that calculates the maximum value in each column of diamonds
-diamonds
+# Use across() to rescale all numeric variables in rcis::worldbank to range between 0 and 1.
+## rescale() function
+rescale01 <- function(x) {
+  rng <- range(x, na.rm = TRUE)
+  (x - rng[1]) / (rng[2] - rng[1])
+}
 
-map_dbl(diamonds, max)
-
-
-
-
+## apply using mutate() and across()
+worldbank %>%
+  mutate(across(.cols = where(is.numeric), .fns = rescale01))
--- 02-iteration.R
+++ 02-iteration.R
@@ -1,21 +1,31 @@
 library(tidyverse)
+library(modeldata)
+library(rcis)
 
 # For loops
-## Write a for loop that calculates the arithmetic mean for every column in mtcars
-as_tibble(mtcars)
+## Write a for loop that calculates the arithmetic mean for every column in car_prices
+car_prices
 
 
 
-## Write a for loop that calculates the maximum value in each column of diamonds
-diamonds
+## Write a for loop that calculates the maximum value in each column of car_prices
+car_prices
 
 
 
 # Write iterative operations using a map() function
-## Write a map() function that calculates the arithmetic mean for every column in mtcars
-as_tibble(mtcars)
+## Write a map() function that calculates the arithmetic mean for every column in car_prices
+car_prices
 
 
-## Write a map() function that calculates the maximum value in each column of diamonds
-diamonds
+## Write a map() function that calculates the maximum value in each column of car_prices
+car_prices
 
+
+
+# Use across() to rescale all numeric variables in rcis::worldbank to range between 0 and 1.
+## rescale() function
+rescale01 <- function(x) {
+  rng <- range(x, na.rm = TRUE)
+  (x - rng[1]) / (rng[2] - rng[1])
+}
